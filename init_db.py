94fe301e80cdb8112829a8aff6d6593099d79052
ryanguydavid38-ops <ryan.guydavid38@gmail.com>
2025-11-17 17:40:37 +0000

    Create init_db.py

---
+++ init_db.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import sqlite3
+import os
+
+# Dossiers utilisés
+DB_FILE = os.path.join("/home/ubuntu/Documents/LAN/network_monitor.db")
+
+def main():
+
+    # Connexion et execution
+    conn = sqlite3.connect(DB_FILE)
+    cur = conn.cursor()
+
+    # Suppression des tables s'ils existent
+    cur.execute("DROP TABLE IF EXISTS http_logs;")
+    cur.execute("DROP TABLE IF EXISTS dns_logs;")
+
+    # Création de la table HTTP
+    cur.execute("""
+    CREATE TABLE http_logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT NOT NULL,   -- date/heure du paquet
+        pcap_file TEXT NOT NULL,   -- nom du fichier pcapng d'origine
+        src_ip TEXT NOT NULL,
+        src_port INTEGER,
+        dst_ip TEXT NOT NULL,
+        dst_port INTEGER,
+        protocol TEXT NOT NULL,    -- HTTP... normalement ^^'
+        info TEXT -- pour un commentaire, fait par l'utilisteur s'il veut
+    );
+    """)
+
+    # Création index pour les requêtes
+    cur.execute("CREATE INDEX idx_http_time ON http_logs(timestamp);")
+    cur.execute("CREATE INDEX idx_http_src_ip ON http_logs(src_ip);")
+    cur.execute("CREATE INDEX idx_http_dst_ip ON http_logs(dst_ip);")
+
+    # Création de la table DNS / mDNS
+    cur.execute("""
+    CREATE TABLE dns_logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT NOT NULL,   -- Meme chose que la table HTTP
+        pcap_file TEXT NOT NULL,   -- Idem
+        src_ip TEXT NOT NULL,      
+        dst_ip TEXT NOT NULL,      
+        query_name TEXT,           -- nom de domaine demandé
+        is_mdns INTEGER            -- 0 = DNS normal, 1 = mDNS (port=5353)
+    );
+    """)
+    
+    # Création idex pour les requêtes
+    cur.execute("CREATE INDEX idx_dns_time ON dns_logs(timestamp);")
+    cur.execute("CREATE INDEX idx_dns_src_ip ON dns_logs(src_ip);")
+    cur.execute("CREATE INDEX idx_dns_qname ON dns_logs(query_name);")
+
+    #Ecrit dans le fichier et ferme la co
+    conn.commit() 
+    conn.close() 
+    print(f"[i] Base SQLite recréée : {DB_FILE}")
+
+if __name__ == "__main__":
+    main()
+

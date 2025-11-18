5ad964f493b518ed2e8c65cb6ca263130884ebce
ryanguydavid38-ops <ryan.guydavid38@gmail.com>
2025-11-18 13:16:25 +0000

    Update surveillance_fichier.py

--- surveillance_fichier.py
+++ surveillance_fichier.py
@@ -4,5 +4,7 @@ from pathlib import Path
 class surveillance:
   def __init__(self, ch_dossier):
     self.dossier = Path(ch_dossier)
-    self.a_fichiers = set(os.listdir
+    self.a_fichiers = set(os.listdir(self.dossier))
     
+test = surveillance("/home/ubuntu/Documents/LAN/LOG")
+print(test.a_fichier)
